f468013c3183dacd074380103a2beacead2ad66d
its-your-nuna <200103140@stu.sdu.edu.kz>
2022-07-13 12:50:34 +0600

    remove route

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import './App.css';
 import { Mainpage } from './components/main/Mainpage';
 import { People } from './components/people/People';
 
-import { Routes, Route } from 'react-router-dom'
-
 
 function App() {
   return (
